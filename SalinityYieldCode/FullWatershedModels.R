e41ddfe654b14428069f5f496be8b20edf67c455
tnaum <naumi421@gmail.com>
2019-03-18 16:15:17 -0600

    Revert "Test commit with change in comments"
    This reverts commit d860fa6844de0dd2052826ab3f83ce5ee470d978.

--- SalinityYieldCode/FullWatershedModels.R
+++ SalinityYieldCode/FullWatershedModels.R
@@ -81,7 +81,7 @@ for(h in hucdivlist){
   frac <- starthuc$FRAC
   startflow <- startreach$Corrected_Q_cms # F1
   flow <- startreach$Corrected_Q_cms*frac #F1,
-  ## New approach: Key assumption that modeled Q is after diversion is removed and needs to be adjusted up at 
+  ## New approach: Key assumption that modeled Q is after diversion and needs to be adjusted up at 
   ## outlet to reflect the diverted flow in creating the load diversion factor.
   startflow_correction = startflow-flow 
   newguage <- ifelse(starthuc$STAID > 0, starthuc$STAID)
